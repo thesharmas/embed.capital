ea8a98498d762fb41001dca1f96835465f1418f9
Rohit <rnsharma@gmail.com>
2021-02-09 13:55:11 -0800

    Initial Commit

---
+++ gitinit.sh
@@ -0,0 +1,7 @@
+git init
+git add .
+git commit -m "Initial Commit"
+git branch -mv master main 
+git remote add origin https://github.com/thesharmas/embed.capital.git
+git branch -M main
+git push -u origin main
